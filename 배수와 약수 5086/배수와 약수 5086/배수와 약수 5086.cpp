ade9d28f792ea3d0bd73b2c05f74e60c09bf8b3d
kmyobin <sowhat0516@gmail.com>
2022-08-18 14:19:13 +0900

    배수, 약수

---
+++ 배수와 약수 5086/배수와 약수 5086/배수와 약수 5086.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	while (1) {
+		int A, B;
+		cin >> A >> B;
+		if (A == 0 && B == 0) break;
+		if (B % A == 0)printf("factor\n");
+		else if (A % B == 0)printf("multiple\n");
+		else printf("neither\n");
+	}
+}
